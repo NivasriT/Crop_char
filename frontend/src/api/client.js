f9071b43ea9c8a7494ccb2d4ab4aa6c509febb28
Nivasri Thirumeni <nivasrithirumeni24@gmail.com>
2026-08-15 06:36:12 +0530

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({ 
-  baseURL: "http://localhost:8000/api" 
+  // If deployed to Render, use "/api". If running locally, use localhost.
+  baseURL: import.meta.env.PROD ? "/api" : "http://localhost:8000/api" 
 });
